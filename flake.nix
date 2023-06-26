b4358f27e923a9381ed6818f38b2a17e5d3b20c5
Break Yang <breakds@gmail.com>
2023-06-26 12:12:40 -0700

    Add nix-based development environment

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Intel® RealSense™ SDK";
+
+  inputs = {
+    # Pointing to the current stable release of nixpkgs. You can
+    # customize this to point to an older version or unstable if you
+    # like everything shining.
+    #
+    # E.g.
+    #
+    # nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux"
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+
+      # Add overlays here if you need to override the nixpkgs
+      # official packages.
+      overlays = [];
+      
+      # Uncomment this if you need unfree software (e.g. cuda) for
+      # your project.
+      #
+      # config.allowUnfree = true;
+    };
+  in {
+    devShells.default = pkgs.mkShell rec {
+      # Update the name to something that suites your project.
+      name = "librealsense";
+
+      packages = with pkgs; [
+        # Development Tools
+        gcc
+        cmake
+        cmakeCurses
+        ninja
+        pkg-config
+
+        # Development time dependencies
+        gtest
+
+        # Build time and Run time dependencies
+        libusb1
+        mesa
+        gtk3
+        glfw
+        libGLU
+        curl
+      ];
+
+      # Setting up the environment variables you need during
+      # development.
+      shellHook = let
+        icon = "f121";
+      in ''
+        export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+      '';
+    };
+
+    # packages.default = pkgs.callPackage ./default.nix {};
+  });
+}
